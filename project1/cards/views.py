56ed22a150d245df04ef1d8af2f1ef1612405912
Hedrox <hedrox53@gmail.com>
2015-07-23 22:23:50 +0300

    Home commit 23.07

--- project1/cards/views.py
+++ project1/cards/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-# Create your views here.
+from .models import Cards
+
+class IndexView(generic.ListView):
+    template_name = 'cards/index.html'
+    context_object_name = 'latest_card_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Cards.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Cards
+    template_name = 'cards/detail.html'
+
--- project1/polls/views.py
+++ project1/polls/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
-from .models import Question
 from django.core.urlresolvers import reverse
 from django.views import generic
 
